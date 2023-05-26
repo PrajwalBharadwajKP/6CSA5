b10eee805905ea213f99558ae18a40cdc1587642
Prajwal <Prajwal@TYSS-Prajwal>
2023-05-26 10:22:09 +0530

    this is my first commit

---
+++ 6CSA5/src/LaunchBrowser/ChromeBrowser.java
@@ -0,0 +1,9 @@
+package LaunchBrowser;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowser {
+	public static void main(String[] args) {
+		ChromeDriver driver = new ChromeDriver();		
+	}
+}
---
+++ 6CSA5/src/LaunchBrowser/EdgeBrowser.java
@@ -0,0 +1,9 @@
+package LaunchBrowser;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class EdgeBrowser {
+	public static void main(String[] args) {
+		EdgeDriver driver=new EdgeDriver();
+	}
+}
