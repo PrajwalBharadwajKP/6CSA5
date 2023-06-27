50af52fb4fadc1e410943d44940bf01fbb414a8c
Prajwal <Prajwal@TYSS-Prajwal>
2023-06-27 14:56:00 +0530

    27-06-2023

---
+++ 6CSA5/src/Sync/ImplicitWait.java
@@ -0,0 +1,17 @@
+package Sync;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ImplicitWait {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofNanos(30));
+		driver.manage().window().maximize();
+		driver.get("https://www.instagram.com/");
+		driver.findElement(By.name("user")).sendKeys("hello");
+	}
+}
